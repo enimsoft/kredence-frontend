a0fddcd149b1d8b9fa81d1a5042e35b78100ff92
Himanshu Tariyal <tariyal2000@gmail.com>
2020-10-06 13:36:20 +0530

    update'

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -39,7 +39,7 @@ function Contact() {
             placeholder="Your Message...."
             className={styles.textarea}
           />
-          <button class="pbutton" type="button">
+          <button class={styles.pbutton} type="button">
             Submit
           </button>
         </div>
