a039eecc13c3c575f1e01b7361c204cb968cf356
Himanshu Tariyal <tariyal2000@gmail.com>
2020-10-28 19:08:58 +0530

    final

---
+++ build/precache-manifest.e15c281197e6ee0fb1118babc4cdb6b0.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4aa135195889a2a350e673061e163f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca4bcbb2292f9e163675",
+    "url": "/static/css/main.48dbe360.chunk.css"
+  },
+  {
+    "revision": "7e9fc86488c2e7912e6f",
+    "url": "/static/js/2.866aaaa9.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.866aaaa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca4bcbb2292f9e163675",
+    "url": "/static/js/main.08082537.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "a5d7538bdc845b35dd0ea4569a8d655d",
+    "url": "/static/media/alta.a5d7538b.png"
+  },
+  {
+    "revision": "bc17e7b78d1871b99eb62b05df2783fe",
+    "url": "/static/media/beaker.bc17e7b7.svg"
+  },
+  {
+    "revision": "d6931e724b3bdd9d0723a1134c6eb744",
+    "url": "/static/media/black.d6931e72.svg"
+  },
+  {
+    "revision": "d49e98d4cb07bef2485f2e6de6505e55",
+    "url": "/static/media/coa.d49e98d4.jpeg"
+  },
+  {
+    "revision": "110f697bdee3688efab26727f84d7268",
+    "url": "/static/media/collag1.110f697b.jpg"
+  },
+  {
+    "revision": "bd2151a45d591132013cdb209368084b",
+    "url": "/static/media/collag2.bd2151a4.png"
+  },
+  {
+    "revision": "996eda15f4f82b864b4767d26f4cc0c0",
+    "url": "/static/media/collage.996eda15.png"
+  },
+  {
+    "revision": "d7e088fc6fdf5e436ed3561ed4dd3201",
+    "url": "/static/media/cpc.d7e088fc.svg"
+  },
+  {
+    "revision": "f418778abad4635e9965b2bd741e2512",
+    "url": "/static/media/drumpquickpro.f418778a.jpg"
+  },
+  {
+    "revision": "cd75629166fbdccaff1aaba91dd64268",
+    "url": "/static/media/elment.cd756291.png"
+  },
+  {
+    "revision": "3ca010b69f5a16c97300faa2bfafa310",
+    "url": "/static/media/elmt1.3ca010b6.png"
+  },
+  {
+    "revision": "659fc537bec734d0caa5c8e35833a8c5",
+    "url": "/static/media/honeywellBJ.659fc537.jpeg"
+  },
+  {
+    "revision": "45528e1d74d9fc91fd955319de83c738",
+    "url": "/static/media/logo.45528e1d.png"
+  },
+  {
+    "revision": "31e48c50a256b03f6725451eb2628957",
+    "url": "/static/media/logo512.31e48c50.png"
+  },
+  {
+    "revision": "5b67d9eba2fa20f1ccbaa109a9889747",
+    "url": "/static/media/p1.5b67d9eb.jpeg"
+  },
+  {
+    "revision": "2392e013be86b170239ee34361b782aa",
+    "url": "/static/media/quality.2392e013.jpg"
+  },
+  {
+    "revision": "43ce9ff45e3cf322ffe8430ba28f57ee",
+    "url": "/static/media/quality.43ce9ff4.svg"
+  },
+  {
+    "revision": "fc9fe4d1676ce6a848293307f71faf8c",
+    "url": "/static/media/setting.fc9fe4d1.svg"
+  },
+  {
+    "revision": "06bcdf6e7a5623f4385cdecd72428a87",
+    "url": "/static/media/solar.06bcdf6e.jpg"
+  }
+]);
