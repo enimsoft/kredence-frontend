6895676a0a94c58ef05e1bc5777d541d6e61334d
Himanshu Tariyal <tariyal2000@gmail.com>
2020-11-05 15:25:15 +0530

    icon

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,11 +3,9 @@ import styles from "./Home.module.css";
 import HomeCarousel from "../../Components/homecarousel/homecarousel";
 import Header from "../../Components/header/Header";
 import cpc from "../../assets/products/cpc/cpc.svg";
-import beaker from "../../assets/beaker.svg";
+import beaker from "../../assets/beaker.jpeg";
 import quality from "../../assets/quality.svg";
 import setting from "../../assets/setting.svg";
-
-import { BrowserRouter as Router, Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -76,15 +74,36 @@ function Home() {
             src={require("../../assets/p1.jpeg")}
             alt=""
           />
-          <p>
-            Having more than 20 years of experience in Performance Chemicals
-            Industry and having worked with Glaxo, Ranbaxy & Avantor Performance
-            Materials India Limited. He worked at various sales positions and
-            was Business Head-Lab Product at Avantor, his last assignment.
-            Awarded M.Phil (Organic Chemistry) degree from Kurukshetra
-            University . Bharat loves to explore new challenges and always
-            having an customer focus approach.
-          </p>
+          <div className={styles.cont}>
+            <p>
+              <span style={{ fontWeight: "bold" }}>Mr Bharat Bhatnagar </span>{" "}
+              has more than 20 years of experience in Performance Chemicals
+              Industry and having worked with Glaxo, Ranbaxy & Avantor
+              Performance Materials India Limited. He worked at various sales
+              positions and was Business Head-Lab Product at Avantor, his last
+              assignment. Awarded M.Phil (Organic Chemistry) degree from
+              Kurukshetra University . Bharat loves to explore new challenges
+              and always having an customer focus approach.
+            </p>
+
+            <ul style={{ padding: "0", listStyle: "none" }}>
+              <li>
+                &#10003; &nbsp; Strategic role in Avantor Performance Materials
+                & part of M&A team of RFCL at the time when Avantor acquire the
+                company.{" "}
+              </li>
+              <li>
+                &#10003; &nbsp; Established Rankem and J.T Baker brand in India
+                by creating a dedicated network of distributors and loyal
+                customers.{" "}
+              </li>
+              <li>
+                &#10003; &nbsp; Developed a effective indigenous solution to
+                Solar cell manufacturing companies for their performance
+                chemicals both in terms of logistic and cost effectiveness.{" "}
+              </li>
+            </ul>
+          </div>
         </div>
 
         <div className={styles.expcard}>
@@ -93,14 +112,34 @@ function Home() {
             src={require("../../assets/p1.jpeg")}
             alt=""
           />
-          <p>
-            Having more than 23 years experience in various functions related to
-            Quality Assurance, Quality Control, Production and Technical
-            Services in performance chemical industry. A holder of Master degree
-            in Chemistry from Kanpur University. He always believe to provide
-            best quality product to customers through quality system and
-            process.
-          </p>
+          <div className={styles.cont}>
+            <p>
+              <span style={{ fontWeight: "bold" }}> Mr Sandeep Mathur</span> has
+              more than 23 years experience in various functions related to
+              Quality Assurance, Quality Control, Production and Technical
+              Services in performance chemical industry. A holder of Master
+              degree in Chemistry from Kanpur University. He always believe to
+              provide best quality product to customers through quality system
+              and process.
+            </p>
+
+            <ul style={{ padding: "0", listStyle: "none" }}>
+              <li>
+                &#10003; &nbsp; Established quality perception of Rankem Brand
+                against EM and TF through Quality systems.{" "}
+              </li>
+              <li>
+                &#10003; &nbsp; Developed loyal customers through sales and
+                marketing team by providing technical inputs and performance of
+                product at customer end.
+              </li>
+              <li>
+                &#10003; &nbsp; Developed the indigenous source of all
+                photovoltaic performance chemicals for solar cell manufacturing
+                customers.{" "}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
 
